681a3333c93c14922e9066eebba136e20d6e9417
William Pescherine <wkpescherine@gmail.com>
2019-05-26 04:21:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
-  return array.join  if array.length == 2
+  if array.length == 2
   array[-2] << " and "
   array.join
   elsif array.length == 1
@@ -9,3 +9,5 @@ def oxford_comma(array)
 	array.join(", ")
   end
 end
+
+oxford_comma(array)
